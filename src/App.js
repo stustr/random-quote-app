d3dc851b853e3de1711816f4733f779e197fb0b4
stustr <sfbsturrock@gmail.com>
2023-01-22 21:39:47 +0000

    test suite removed for deployment

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react";
 import "./App.scss";
 import axios from "axios";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 function App() {
   const [author, setAuthor] = useState();
